5e28c3b3e21d853ee03211379da9fbd9495da1ed
Pascal Heimann <Pascal@DESKTOP-N38CJ2S.fritz.box>
2021-03-21 19:39:47 +0100

    selber kek

--- SlickGame/src/GameStates/Main.java
+++ SlickGame/src/GameStates/Main.java
@@ -18,7 +18,7 @@ public class Main extends StateBasedGame{
 	public static void main(String[] args) throws SlickException {
 		
 		
-		System.out.println("kek");
+		System.out.println("selber kek");
 		
 		AppGameContainer container = new AppGameContainer(new Main());
 		container.setDisplayMode(container.getScreenWidth(), container.getScreenHeight(), true);
