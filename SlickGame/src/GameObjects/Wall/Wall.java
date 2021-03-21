d29b2bfc712c3c780395736799364eed931f2a94
Pascal Heimann <Pascal@DESKTOP-N38CJ2S.fritz.box>
2021-03-21 18:02:56 +0100

    remove imports

--- SlickGame/src/GameObjects/Wall/Wall.java
+++ SlickGame/src/GameObjects/Wall/Wall.java
@@ -3,13 +3,10 @@ package GameObjects.Wall;
 import org.newdawn.slick.Color;
 import org.newdawn.slick.GameContainer;
 import org.newdawn.slick.Graphics;
-import org.newdawn.slick.geom.Rectangle;
-import org.newdawn.slick.geom.Shape;
 import org.newdawn.slick.state.StateBasedGame;
 
 import GameObjects.GameObject;
 import GameStates.MyBasicGameState;
-import idk.Vector2D;
 
 public class Wall extends GameObject {
 	
